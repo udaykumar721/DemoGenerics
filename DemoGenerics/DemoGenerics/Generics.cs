2477b3512f4ff2cfe522f82006839d41f389d6b8
udaykumar721 <uday56367@gmail.com.com>
2023-09-27 09:02:18 +0530

    [uday]Add : Generics Demo.

---
+++ DemoGenerics/DemoGenerics/Generics.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoGenerics
+{
+    //Generics Means Genreral Form
+    //it doesnot specify a particular data typa
+    //Generic allow us to a single class or method that can be us for different data types
+    //<T> with type parameter
+    //type parameter 
+    internal class Generics <T>
+    {
+        //public string data ;
+        public T data;
+        public Generics(T data)
+        {
+            this.data = data;
+            Console.WriteLine(this.data);
+        }
+        public void DemoMethod(T no)
+        {
+            Console.WriteLine(no);
+        }
+    }
+    public class Generic
+    {
+        public void GenericMethod<T>(T num)
+        {
+            Console.WriteLine(num);
+        }
+    }
+}
---
+++ DemoGenerics/DemoGenerics/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoGenerics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Generics<int> generics = new Generics<int>(10);
+            Generics<string> genericString = new Generics<string>("uday");
+            Generics<double> genericDouble = new Generics<double>(10.09);
+            Console.WriteLine();
+            generics.DemoMethod(721);
+            genericString.DemoMethod("kumar");
+            genericDouble.DemoMethod(99.45);
+            Generic generic = new Generic();
+            generic.GenericMethod(5);
+            generic.GenericMethod("uday");
+            generic.GenericMethod(93.94);
+            generic.GenericMethod<int>(5);
+            generic.GenericMethod<string>("uday");
+            generic.GenericMethod<double>(93.94);
+            Console.ReadLine();
+        }
+    }
+}
